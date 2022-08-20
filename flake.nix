c81d13f96294ab9d625c77fd5fdfeeb17f3fb8ab
mitchmindtree <mail@mitchellnordine.com>
2022-08-21 00:26:07 +1000

    Update to blackbox 0.12.0. Add libadwaita override for 1.2.beta.
    The libadwaita override is required as blackbox 0.12 requires libadwaita
    1.2 or greater, though nixpkgs unstable only hosts 1.1.4.

--- flake.nix
+++ flake.nix
@@ -21,8 +21,26 @@
 
   outputs = inputs: let
     system = "x86_64-linux";
-    pkgs = inputs.nixpkgs.legacyPackages.${system};
+    overlays = [inputs.self.overlays.default];
+    pkgs = import inputs.nixpkgs {inherit overlays system;};
   in {
+    overlays = {
+      blackbox = final: prev: {
+        # Requires libadwaita 1.2, but nixpkgs doesn't have this yet.
+        libadwaita = prev.libadwaita.overrideAttrs (old: rec {
+          version = "1.2.beta";
+          src = prev.fetchFromGitLab {
+            domain = "gitlab.gnome.org";
+            owner = "GNOME";
+            repo = "libadwaita";
+            rev = version;
+            hash = "sha256-QBblkeNAgfHi5YQxaV9ceqNDyDIGu8d6pvLcT6apm6o=";
+          };
+        });
+      };
+      default = inputs.self.overlays.blackbox;
+    };
+
     packages.${system} = rec {
       vte-gtk4 = pkgs.callPackage ./vte-gtk4.nix {
         inherit (inputs) vte-src;
